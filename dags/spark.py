8d85edaac5d2a75da9cec6509ce01bd4a179fcdf
Milan Jovanovic <milan.jovanovic@cubic.com>
2024-02-02 15:55:53 +0100

    Test

--- dags/spark.py
+++ dags/spark.py
@@ -19,27 +19,11 @@ spark_dag = DAG(
 )
 
 Extract = SparkSubmitOperator(
-		application = "/home/docker/docker-spark-airflow/dags/spark_etl_script_docker.py",
+		application = "wordcount.py",
 		conn_id= 'spark_local', 
 		task_id='spark_submit_task', 
-		dag=spark_dag
+        files='data.txt',
+		dag=spark_dag,
+        application_args='data.txt'
 		)
-
-# New DAG for Kaggle data extraction
-kaggle_dag = DAG(
-    dag_id="kaggle_data_extraction_dag",
-    default_args=default_args,
-    schedule_interval="0 0 * * *",  # Define the schedule for daily execution at midnight
-    dagrun_timeout=timedelta(minutes=60),
-    description='DAG for extracting data from Kaggle',
-    start_date=airflow.utils.dates.days_ago(1)
-)
-
-# Extract data from Kaggle using SparkSubmitOperator (replace the application path and parameters accordingly)
-kaggle_extract = SparkSubmitOperator(
-    application="/path/to/your/kaggle_extraction_script.py",
-    conn_id='spark_local',  # Adjust connection ID if needed
-    task_id='kaggle_data_extraction_task',
-    dag=kaggle_dag
-)
 
