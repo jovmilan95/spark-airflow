c10c820ac451690bcfc06f0e9a1bfca9d3f75555
Milan Jovanovic <milan.jovanovic@cubic.com>
2024-02-02 14:48:14 +0100

    Update DAG name

--- dags/spark.py
+++ dags/spark.py
@@ -38,7 +38,7 @@ from airflow.operators.bash import BashOperator
 
 # [START instantiate_dag]
 with DAG(
-    "tutorial",
+    "spark_example",
     # [START default_args]
     # These args will get passed on to each operator
     # You can override them on a per-task basis during operator initialization
